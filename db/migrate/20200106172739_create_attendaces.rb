8df5187bfae8dedf60ebe669d87af8c8d037cce4
Christian Salazar <calaca89@gmail.com>
2020-01-06 12:29:22 -0500

    setup join table calls attendance

---
+++ db/migrate/20200106172739_create_attendaces.rb
@@ -0,0 +1,10 @@
+class CreateAttendaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :attendaces do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
